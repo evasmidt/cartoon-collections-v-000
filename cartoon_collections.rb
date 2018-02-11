7bad651e39677a7e59701dc04ef83b78b94a5eae
Eva Smidt <shevangeline@gmail.com>
2018-02-11 00:36:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,6 @@ def long_planeteer_calls(calls)# code an argument here
     planeteer_calls.any?  { |call| call.length > 4}
 
 end
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
